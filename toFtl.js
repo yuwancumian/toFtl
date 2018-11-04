1cfbe8e08dafd4ade8bf211a83401866362328cf
yuwancumian <yuwancumian@hotmail.com>
2018-11-04 16:21:40 +0800

    use sed

--- toFtl.js
+++ toFtl.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 require('shelljs/global');
-console.log('hello')
+ls('*.html').forEach(function(file){
+  console.log(file);
+  sed('-i', '<extend name="Layout/ins_page" />', '', file);
+  sed('-i', '<block name="content">', '<@override name="content">', file)
+  sed('-i', '<block name="js">', '<@override name="js">', file)
+  sed('-i', '</block>', '</@override>', file)
+  '<@extends name="../layout/ins_page.html"/>\n'.toEnd(file)
+  echo(cat(file));
+})
